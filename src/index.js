dcc1d4dde61ad87817792b9a6403f2ed31b38fc8
Mosh Hamedani <moshfegh@live.com.au>
2018-06-27 12:42:14 -0700

    Creating your first component.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Counter />, document.getElementById("root"));
 registerServiceWorker();
