73f8a655ab56efc66772c32ef3de7cbe9b05bf58
Mosh Hamedani <moshfegh@live.com.au>
2018-06-28 11:04:14 -0700

    Setting attributes.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,8 +8,8 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
