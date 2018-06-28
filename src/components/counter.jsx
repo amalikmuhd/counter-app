fc38a2c4c9e72db2bc0f54c80140a7a75c7adcf4
Mosh Hamedani <moshfegh@live.com.au>
2018-06-28 10:26:43 -0700

    Specifying children.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </div>
+    );
   }
 }
 
