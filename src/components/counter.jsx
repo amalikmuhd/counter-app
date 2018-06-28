861921ddab6d25776a1d93cb1e7d1a367e33da89
Mosh Hamedani <moshfegh@live.com.au>
2018-06-28 10:44:08 -0700

    Specifying children.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </div>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
